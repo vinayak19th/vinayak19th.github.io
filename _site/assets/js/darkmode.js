94e69b4a1744588c4bc23897120eea0387a5d343
vinayak19th <vinayak1998th@gmail.com>
2024-03-03 23:07:44 -0700

    Moved darkmode to icon

--- _site/assets/js/darkmode.js
+++ _site/assets/js/darkmode.js
@@ -1,10 +1,13 @@
 const theme = localStorage.getItem('theme');
+const darkicon = '<i class="fa-solid fa-moon darkicon"></i>'
+// const lighticon = '<i class="fa-solid fa-circle-half-stroke"></i>'
+const lighticon = '<i class="fa-solid fa-sun"></i>'
 
 if (theme == "dark") {
     document.documentElement.setAttribute('data-theme', 'dark');
     var element = document.body;
     element.classList.toggle("dark");
-    document.getElementById("theme-toggle").innerHTML = "Light Mode";
+    document.getElementById("theme-toggle").innerHTML = lighticon;
     var items = document.getElementsByClassName("GitData");
     for (var i = 0; i < items.length; i++) {
         items[i].src = items[i].src.replace('ocean_dark', 'tokyonight');
@@ -13,17 +16,17 @@ if (theme == "dark") {
 const userPrefers = getComputedStyle(document.documentElement).getPropertyValue('content');
 
 if (theme === "dark") {
-    document.getElementById("theme-toggle").innerHTML = "Light Mode";
+    document.getElementById("theme-toggle").innerHTML = lighticon;
 } else if (theme === "light") {
-    document.getElementById("theme-toggle").innerHTML = "Dark Mode";
+    document.getElementById("theme-toggle").innerHTML = darkicon;
 } else if (userPrefers === "dark") {
     document.documentElement.setAttribute('data-theme', 'dark');
     window.localStorage.setItem('theme', 'dark');
-    document.getElementById("theme-toggle").innerHTML = "Light Mode";
+    document.getElementById("theme-toggle").innerHTML = lighticon;
 } else {
     document.documentElement.setAttribute('data-theme', 'light');
     window.localStorage.setItem('theme', 'light');
-    document.getElementById("theme-toggle").innerHTML = "Dark Mode";
+    document.getElementById("theme-toggle").innerHTML = darkicon;
 }
 
 function modeSwitcher() {
@@ -33,7 +36,7 @@ function modeSwitcher() {
     if (currentMode == "dark") {
         document.documentElement.setAttribute('data-theme', 'light');
         window.localStorage.setItem('theme', 'light');
-        document.getElementById("theme-toggle").innerHTML = "Dark Mode";
+        document.getElementById("theme-toggle").innerHTML = darkicon;
         var items = document.getElementsByClassName("GitData");
         for (var i = 0; i < items.length; i++) {
             items[i].src = items[i].src.replace('ocean_dark', 'tokyonight');
@@ -41,7 +44,7 @@ function modeSwitcher() {
     } else {
         document.documentElement.setAttribute('data-theme', 'dark');
         window.localStorage.setItem('theme', 'dark');
-        document.getElementById("theme-toggle").innerHTML = "Light Mode";
+        document.getElementById("theme-toggle").innerHTML = lighticon;
         var items = document.getElementsByClassName("GitData");
         for (var i = 0; i < items.length; i++) {
             items[i].src = items[i].src.replace('tokyonight', 'ocean_dark');
@@ -52,5 +55,5 @@ function modeSwitcher() {
 //Check User Device Settings
 /*if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
     document.body.classList.add("dark");
-    document.getElementById("theme-toggle").innerHTML = "Light Mode";
+    document.getElementById("theme-toggle").innerHTML = lighticon;
 }*/
