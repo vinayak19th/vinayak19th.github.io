9e71b7799c1d5a224f1d4a4f8534aa8c009738ce
vinayak19th <vinayak1998th@Gmail.com>
2026-02-04 22:27:16 -0700

    New navbar styling

---
+++ assets/js/sidebar.js
@@ -0,0 +1,13 @@
+'use strict';
+
+// element toggle function
+const elementToggleFunc = function (elem) { elem.classList.toggle("active"); }
+
+// sidebar variables
+const sidebar = document.querySelector("[data-sidebar]");
+const sidebarBtn = document.querySelector("[data-sidebar-btn]");
+
+// sidebar toggle functionality for mobile
+if (sidebar && sidebarBtn) {
+    sidebarBtn.addEventListener("click", function () { elementToggleFunc(sidebar); });
+}
